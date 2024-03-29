faf95672f2ce4e7bde8e50d4c6ef946bc95fd7ef
unknown <@ngknn.local>
2024-03-29 15:59:53 +0300

    init

---
+++ UnitDLL/Program.cs
@@ -0,0 +1,13 @@
+﻿using LibraryDLL;
+
+namespace UnitDLL
+{
+    public class Program
+    {
+        static void Main()
+        {
+            СheckSumDLL obj = new СheckSumDLL("JHMCM56557C404453");
+            Console.WriteLine(obj.ExaminationСheckSum());
+        }
+    }
+}
